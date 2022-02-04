8d1b669bf2f3846b6d762941e21c65484cd78d8f
Thibault <houdon.thibault@gmail.com>
2022-02-04 14:32:02 +0100

    Fix starting files

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,27 +3,16 @@ from django.shortcuts import render
 
 from accounts.models import CustomUser
 
-from django.contrib.auth.forms import UserCreationForm
-
-
-class CustomSignupForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields
-
 
 def signup(request):
-    context = {}
-
     if request.method == "POST":
-        form = CustomSignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse("Bienvenue !")
-        else:
-            context["errors"] = form.errors
-
-    form = CustomSignupForm()
-    context["form"] = form
-
-    return render(request, "accounts/signup.html", context=context)
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        if password1 != password2:
+            return render(request, "accounts/signup.html", {"error": "Les mots de passe ne correspondent pas"})
+    
+        CustomUser.objects.create_user(username=username, password=password1)
+        return HttpResponse(f"Bienvenue {username} !")
+    
+    return render(request, "accounts/signup.html")
